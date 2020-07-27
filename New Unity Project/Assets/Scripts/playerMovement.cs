7c8763520ff7b7243a16229129c981572fdce620
Matt Ryan <matthewtryan99@gmail.com>
2020-07-26 20:41:38 -0500

    player shooting

--- New Unity Project/Assets/Scripts/playerMovement.cs
+++ New Unity Project/Assets/Scripts/playerMovement.cs
@@ -7,6 +7,9 @@ public class playerMovement : MonoBehaviour
 
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 0.5f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+
     float xMin;
     float xMax;
     float yMin;
@@ -18,18 +21,11 @@ public class playerMovement : MonoBehaviour
         MoveBoundries();
     }
 
-    private void MoveBoundries(){
-        Camera gameCamera = Camera.main;
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).x + padding;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1,0,0)).x - padding;
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).y + padding;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0,1,0)).y - padding;
-    }
-
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
     }
 
     private void Move()
@@ -40,5 +36,22 @@ public class playerMovement : MonoBehaviour
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         var newY = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newX, newY);
+    }
+
+        private void MoveBoundries(){
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1,0,0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0,1,0)).y - padding;
+    }
+
+    private void Fire()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+            laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+        }
     }
 }
